9b6a25f9b30cad3682082d14faf40227958214ad
verben-a <alina.verbenchuk@gmail.com>
2017-04-14 20:36:43 +0100

    Fixed hides

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // // Code goes here
 angular.module('myApp', [])
     .controller('myController', function() {
-            var vm = this; // Is some kind of "object" --> $scope
-            vm.name = "";
-            vm.jobtitle = "";
-            vm.tedioustask = "";
-            vm.dirtytask = "";
-            vm.celebrity = "";
-            vm.uselessskill = "";
-            vm.adjective = "";
-            vm.obnoxiuouscelbertity = "";
-        }
 
-     // end of array
-); // end of "controller"
+        var vm = this; 
+
+        vm.name = "";
+        vm.jobtitle = "";
+        vm.tedioustask = "";
+        vm.dirtytask = "";
+        vm.celebrity = "";
+        vm.uselessskill = "";
+        vm.adjective = "";
+        vm.obnoxiuouscelbertity = "";
+
+
+        vm.submitted = false;
+
+    });
