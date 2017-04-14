d1f57db982c9a5518e7636d4d4b8de0c6d4cd656
verben-a <alina.verbenchuk@gmail.com>
2017-04-14 14:57:42 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // // Code goes here
 angular.module('myApp', [])
-.controller('myController', ['$scope',function myController($scope) {
-	
-}]);
+    .controller('myController', function() {
+            var vm = this; // Is some kind of "object" --> $scope
+            vm.name = "";
+            vm.jobtitle = "";
+            vm.tedioustask = "";
+            vm.dirtytask = "";
+            vm.celebrity = "";
+            vm.uselessskill = "";
+            vm.adjective = "";
+            vm.obnoxiuouscelbertity = "";
+        }
+
+     // end of array
+); // end of "controller"
